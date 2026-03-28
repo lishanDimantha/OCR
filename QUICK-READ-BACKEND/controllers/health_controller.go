4d34e5ba23e633ad637446b96e8e69a44ad4a388
Lishan Dimantha <lishandimantha2020@gmail.com>
2026-03-27 21:20:39 -0700

    fix parth error

--- QUICK-READ-BACKEND/controllers/health_controller.go
+++ QUICK-READ-BACKEND/controllers/health_controller.go
@@ -1,16 +1,17 @@
 package controllers
 
 import (
+	"QUICK-READ-BACKEND/config"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
-	"github.com/lishanDimantha/OCR/QUICK-READ-BACKEND/config" // ඔබේ config පැකේජය මෙතැනට යොදන්න
 )
 
 func HealthCheck(c *gin.Context) {
 	// 1. Database එක වැඩ කරනවාදැයි පරීක්ෂා කිරීම
 	sqlDB, err := config.DB.DB()
 	dbStatus := "OK"
-	
+
 	if err != nil || sqlDB.Ping() != nil {
 		dbStatus = "Disconnected"
 	}
